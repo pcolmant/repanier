50b31b51b182a3a52279fc4d4e701f8e548cb33b
Patrick <pcolmant@gmail.com>
2015-11-04 21:10:29 +0100

    The value of 'list_editable' must be a list or tuple

--- repanier/admin.py
+++ repanier/admin.py
@@ -888,7 +888,7 @@ class ProductAdmin(ImportExportMixin, TranslatableAdmin):
     list_display = ('is_into_offer', 'producer','department_for_customer', 'get_long_name', 'producer_unit_price',
                 'unit_deposit', 'customer_alert_order_quantity', 'stock')
     list_display_links = ('get_long_name',)
-    list_editable = None
+    list_editable = ('producer_unit_price', 'stock')
     readonly_fields = ('is_created_on',
                        'is_updated_on')
     list_select_related = ('producer', 'department_for_customer')
@@ -923,7 +923,6 @@ class ProductAdmin(ImportExportMixin, TranslatableAdmin):
             producer = producer_queryset.first()
         else:
             producer = None
-        self.list_editable = ('producer_unit_price', 'stock')
         if producer is not None:
             if producer.manage_stock:
                 return ('is_into_offer', 'producer','department_for_customer', 'get_long_name', 'all_languages_column', 'producer_unit_price',
