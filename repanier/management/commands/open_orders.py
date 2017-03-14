9de5f81c04c25281b4b86da82fe4305c31aabf60
Patrick <pcolmant@gmail.com>
2017-03-14 19:16:03 +0100

    cron job to open orders

---
+++ repanier/management/commands/open_orders.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from repanier.task import task_order
+
+
+class Command(BaseCommand):
+    args = '<none>'
+    help = 'Open pre opened orders'
+
+    def handle(self, *args, **options):
+        something_to_open = task_order.automatically_open()
+        if something_to_open:
+            self.stdout.write('At least one order being opened')
+        else:
+            self.stdout.write('Nothing to open')
