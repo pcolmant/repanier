0577290b6bdd43f908969e5e02ca4b6ac32a5f7c
Patrick <pcolmant@gmail.com>
2017-03-27 19:21:47 +0200

    Display into the admin if the user is a tester

--- repanier/models/staff.py
+++ repanier/models/staff.py
@@ -77,7 +77,8 @@ class Staff(MPTTModel, TranslatableModel):
     @property
     def title_for_admin(self):
         if self.customer_responsible is not None:
-            return '%s : %s (%s)' % (self.safe_translation_getter('long_name', any_language=True), self.customer_responsible.long_basket_name, self.customer_responsible.phone1)
+            tester = _(" who is also tester ") if self.is_tester else EMPTY_STRING
+            return '%s : %s%s (%s)' % (self.safe_translation_getter('long_name', any_language=True), self.customer_responsible.long_basket_name, tester, self.customer_responsible.phone1)
         else:
             return '%s' % self.safe_translation_getter('long_name', any_language=True)
 
