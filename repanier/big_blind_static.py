a9d38a99933ed4ade9f4e4d5967b0c45b2e61701
Patrick <pcolmant@gmail.com>
2017-04-22 15:46:33 +0200

    Solve the reverted https://github.com/divio/django-cms/pull/5860/

--- repanier/big_blind_static.py
+++ repanier/big_blind_static.py
@@ -1,10 +1,17 @@
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
-
+from cms.utils.urlutils import static_with_version
+import cms
+__version__ = "/%s" % cms.__version__
 
 class BigBlindManifestStaticFilesStorage(ManifestStaticFilesStorage):
 
-    def url(self, name, force=True):
+    def url(self, context, force=True):
         """
         Override .url to use hashed url in development
         """
-        return super(ManifestStaticFilesStorage, self).url(name, True)
+        try:
+            return super(ManifestStaticFilesStorage, self).url(context, True)
+        except ValueError:
+            # Solve the reverted https://github.com/divio/django-cms/pull/5860/
+            context = static_with_version(context)
+            return super(ManifestStaticFilesStorage, self).url(context, True).replace(__version__, "")
