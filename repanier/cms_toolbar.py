fc3032884f9ff7d1f9158148dc807a0d0ade4647
Patrick <pcolmant@gmail.com>
2017-07-20 18:01:41 +0200

    Add notification, remove one unused query and box

--- repanier/cms_toolbar.py
+++ repanier/cms_toolbar.py
@@ -9,7 +9,6 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from repanier.const import *
-from repanier.models import Configuration
 
 
 @toolbar_pool.register
@@ -19,20 +18,28 @@ class RepanierToolbar(CMSToolbar):
         if settings.DJANGO_SETTINGS_DEMO:
             self.toolbar.get_or_create_menu("demo-menu", _('Demo (%s)') % (DEMO_EMAIL,))
         user = self.request.user
+        is_in_order_group = False
+        is_in_invoice_group = False
         if user.is_superuser or user.groups.filter(
                 name=COORDINATION_GROUP).exists():
             display_all = True
             display_configuration = True
-        elif user.groups.filter(
-                name__in=[ORDER_GROUP, INVOICE_GROUP]).exists():
-            display_all = True
-            display_configuration = False
-        elif user.groups.filter(
-                name=CONTRIBUTOR_GROUP).exists():
-            display_all = False
-            display_configuration = False
+            is_in_order_group = True
+            is_in_invoice_group = True
         else:
-            return
+            if user.groups.filter(name=ORDER_GROUP).exists():
+                is_in_order_group=True
+            if user.groups.filter(name=INVOICE_GROUP).exists():
+                is_in_invoice_group=True
+            if is_in_order_group or is_in_invoice_group:
+                display_all = True
+                display_configuration = False
+            elif user.groups.filter(
+                    name=CONTRIBUTOR_GROUP).exists():
+                display_all = False
+                display_configuration = False
+            else:
+                return
         admin_menu = self.toolbar.get_or_create_menu(ADMIN_MENU_IDENTIFIER, _('Manage'))
         position = 0
         admin_menu.add_break('custom-break', position=position)
@@ -44,9 +51,12 @@ class RepanierToolbar(CMSToolbar):
             )
             # add_sideframe_item
             if display_configuration:
-                config = Configuration.objects.filter(id=DECIMAL_ONE).only('id').first()
-                url = reverse('admin:repanier_configuration_change', args=(config.id,))
+                # config = Configuration.objects.filter(id=DECIMAL_ONE).only('id').first()
+                url = reverse('admin:repanier_configuration_change', args=(1,))
                 office_menu.add_sideframe_item(_('Configuration'), url=url)
+            url = reverse('admin:repanier_notification_change', args=(1,))
+            office_menu.add_sideframe_item(_('Notification'), url=url)
+            if display_configuration:
                 url = reverse('admin:repanier_staff_changelist')
                 office_menu.add_sideframe_item(_('Staff Member List'), url=url)
                 if not settings.DJANGO_SETTINGS_IS_MINIMALIST:
@@ -70,30 +80,29 @@ class RepanierToolbar(CMSToolbar):
         admin_menu.add_sideframe_item(_('Producer List'), url=url, position=position)
 
         if display_all:
-            if not settings.DJANGO_SETTINGS_IS_MINIMALIST:
+            # if not settings.DJANGO_SETTINGS_IS_MINIMALIST:
+            #     position += 1
+            #     url = "%s?is_into_offer__exact=1" % reverse('admin:repanier_box_changelist')
+            #     admin_menu.add_sideframe_item(_('Box List'), url=url, position=position)
+            if is_in_order_group:
                 position += 1
-                url = "%s?is_into_offer__exact=1" % reverse('admin:repanier_box_changelist')
-                admin_menu.add_sideframe_item(_('Box List'), url=url, position=position)
-
-            position += 1
-            url = reverse('admin:repanier_permanenceinpreparation_changelist')
-            admin_menu.add_sideframe_item(
-                _("%(name)s in preparation list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
-                url=url, position=position)
-
-            if REPANIER_SETTINGS_INVOICE:
-                position += 1
-                url = reverse('admin:repanier_permanencedone_changelist')
+                url = reverse('admin:repanier_permanenceinpreparation_changelist')
                 admin_menu.add_sideframe_item(
-                    _("%(name)s done list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
+                    _("%(name)s in preparation list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
                     url=url, position=position)
-
-                position += 1
-                url = reverse('admin:repanier_bankaccount_changelist')
-                admin_menu.add_sideframe_item(_('Bank Account List'), url=url, position=position)
-            else:
-                position += 1
-                url = reverse('admin:repanier_permanencedone_changelist')
-                admin_menu.add_sideframe_item(
-                    _("%(name)s archived list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
-                    url=url, position=position)
+            if is_in_invoice_group:
+                if REPANIER_SETTINGS_INVOICE:
+                    position += 1
+                    url = reverse('admin:repanier_permanencedone_changelist')
+                    admin_menu.add_sideframe_item(
+                        _("%(name)s done list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
+                        url=url, position=position)
+                    position += 1
+                    url = reverse('admin:repanier_bankaccount_changelist')
+                    admin_menu.add_sideframe_item(_('Bank Account List'), url=url, position=position)
+                else:
+                    position += 1
+                    url = reverse('admin:repanier_permanencedone_changelist')
+                    admin_menu.add_sideframe_item(
+                        _("%(name)s archived list") % {'name': REPANIER_SETTINGS_PERMANENCES_NAME},
+                        url=url, position=position)
