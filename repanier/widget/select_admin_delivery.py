802ccc9272ab93973460f85b5ab64fa3e33a90cf
Patrick Colmant <pcolmant@gmail.com>
2017-11-11 10:33:02 +0100

    Align "select admin purchase qty"

--- repanier/widget/select_admin_delivery.py
+++ repanier/widget/select_admin_delivery.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8
 
 from django import forms
+from django.utils.safestring import mark_safe
 
 from repanier.const import PERMANENCE_OPENED, PERMANENCE_CLOSED, PERMANENCE_SEND, PERMANENCE_PLANNED
 from repanier.models.deliveryboard import DeliveryBoard
 
 
 class SelectAdminDeliveryWidget(forms.Select):
-    template_name = 'repanier/widgets/select_admin_delivery.html'
+    template_name = 'repanier/widgets/select_admin_purchase_qty.html'
 
     def get_context(self, name, value, attrs):
         context = super(SelectAdminDeliveryWidget, self).get_context(name, value, attrs)
-        case_show_show = 'case "0": '
-        case_show_hide = 'case "0": '
-        case_hide_show = 'case "0": '
+        case_show_show = "case \"0\": "
+        case_show_hide = "case \"0\": "
+        case_hide_show = "case \"0\": "
         for option_value, option_label in self.choices:
             status = DeliveryBoard.objects.filter(id=option_value).order_by('?').only('status').first().status
             if status in [PERMANENCE_PLANNED, PERMANENCE_OPENED, PERMANENCE_CLOSED]:
@@ -22,7 +23,7 @@ class SelectAdminDeliveryWidget(forms.Select):
                 case_hide_show += "case \"{}\": ".format(option_value)
             else:
                 case_show_show += "case \"{}\": ".format(option_value)
-        context['case_show_show'] = case_show_show
-        context['case_show_hide'] = case_show_hide
-        context['case_hide_show'] = case_hide_show
+        context["case_show_show"] = mark_safe(case_show_show)
+        context["case_show_hide"] = mark_safe(case_show_hide)
+        context["case_hide_show"] = mark_safe(case_hide_show)
         return context
--- repanier/widget/select_admin_permanence.py
+++ repanier/widget/select_admin_permanence.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8
 
 from django import forms
+from django.utils.safestring import mark_safe
 
 from repanier.const import PERMANENCE_OPENED, PERMANENCE_CLOSED, PERMANENCE_SEND, PERMANENCE_PLANNED
 from repanier.models.permanence import Permanence
 
 
 class SelectAdminPermanenceWidget(forms.Select):
-    template_name = 'repanier/widgets/select_admin_permanence.html'
+    template_name = 'repanier/widgets/select_admin_purchase_qty.html'
 
     def get_context(self, name, value, attrs):
         context = super(SelectAdminPermanenceWidget, self).get_context(name, value, attrs)
-        case_show_hide = 'case "0": '
-        case_hide_show = 'case "0": '
-        case_hide_hide = 'case "0": '
+        case_show_show = "case \"0\": "
+        case_show_hide = "case \"0\": "
+        case_hide_show = "case \"0\": "
         for option_value, option_label in self.choices:
             permanence = Permanence.objects.filter(id=option_value).order_by('?').only('status').first()
             if permanence is not None:
@@ -23,8 +24,8 @@ class SelectAdminPermanenceWidget(forms.Select):
                 elif status == PERMANENCE_SEND:
                     case_hide_show += "case \"{}\": ".format(option_value)
                 else:
-                    case_hide_hide += "case \"{}\": ".format(option_value)
-        context['case_show_hide'] = case_show_hide
-        context['case_hide_show'] = case_hide_show
-        context['case_hide_hide'] = case_hide_hide
+                    case_show_show += "case \"{}\": ".format(option_value)
+        context["case_show_show"] = mark_safe(case_show_show)
+        context["case_show_hide"] = mark_safe(case_show_hide)
+        context["case_hide_show"] = mark_safe(case_hide_show)
         return context
