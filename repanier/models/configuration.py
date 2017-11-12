84b191a6b081e5f67ec89004a1589489713cfd22
Patrick Colmant <pcolmant@gmail.com>
2017-11-12 11:31:56 +0100

    Use save_translations() instead of save()

--- repanier/models/configuration.py
+++ repanier/models/configuration.py
@@ -296,7 +296,7 @@ class Configuration(TranslatableModel):
                     <br>
                     {{ signature }}
                     """
-                self.save()
+                self.save_translations()
             except TranslationDoesNotExist:
                 pass
 
