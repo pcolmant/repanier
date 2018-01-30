b87ca2b848fb6d0f1565a3ba1f2a1588740a2dce
Patrick Colmant <pcolmant@gmail.com>
2018-01-30 17:38:39 +0100

    Restore Thread usage when closing the orders

--- repanier/task/task_order.py
+++ repanier/task/task_order.py
@@ -251,10 +251,10 @@ def close_and_send_order(permanence_id, everything=True, producers_id=(), delive
 
 
 def admin_close_and_send_order(permanence_id, everything=True, producers_id=(), deliveries_id=()):
-    close_and_send_order(permanence_id, everything, producers_id, deliveries_id)
-    # t = threading.Thread(target=close_and_send_order,
-    #                      args=(permanence_id, everything, producers_id, deliveries_id))
-    # t.start()
+    # close_and_send_order(permanence_id, everything, producers_id, deliveries_id)
+    t = threading.Thread(target=close_and_send_order,
+                         args=(permanence_id, everything, producers_id, deliveries_id))
+    t.start()
     user_message = _("The orders are being send.")
     user_message_level = messages.INFO
     return user_message, user_message_level
