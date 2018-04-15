599353c5fe8e617705b1f8228c17d6496425b75c
Patrick Colmant <pcolmant@gmail.com>
2018-04-15 17:44:11 +0200

    Workaround concerning upgrade to djangocms-text-ckeditor 3.6.0.

--- repanier/big_blind_static.py
+++ repanier/big_blind_static.py
@@ -1,7 +1,9 @@
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
-from cms.utils.urlutils import static_with_version
 import cms
+from cms.utils.urlutils import static_with_version
+from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+
 __version__ = "/{}".format(cms.__version__)
+
 
 class BigBlindManifestStaticFilesStorage(ManifestStaticFilesStorage):
 
@@ -14,4 +16,9 @@ class BigBlindManifestStaticFilesStorage(ManifestStaticFilesStorage):
         except ValueError:
             # Solve the reverted https://github.com/divio/django-cms/pull/5860/
             context = static_with_version(context)
-            return super(ManifestStaticFilesStorage, self).url(context, True).replace(__version__, "")
+            try:
+                return super(ManifestStaticFilesStorage, self).url(context, True).replace(__version__, "")
+            except ValueError:
+                if context == "djangocms_text_ckeditor/js/dist/bundle-45a646fecc.cms.ckeditor.min.js":
+                    context = "djangocms_text_ckeditor/js/dist/bundle-bca0d2d3f4.cms.ckeditor.min.js"
+                return super(ManifestStaticFilesStorage, self).url(context, True).replace(__version__, "")
