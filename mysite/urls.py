0f08f476dbacaa673c382cb078a0f0106f5a4c15
pcolmant <pcolmant@gmail.com>
2019-06-07 14:25:42 +0200

    Migration to Django 2.x

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,4 @@
+import django
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
 from django.conf.urls import include, url
@@ -7,19 +8,42 @@ from django.contrib.sitemaps.views import sitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.i18n import JavaScriptCatalog
 
-urlpatterns = i18n_patterns(
-    url(r'^repanier/', include('repanier.urls')),
-    url(r'^coordi/', include(admin.site.urls)),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}},
-        name='django.contrib.sitemaps.views.sitemap'),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-    url(r'^', include('cms.urls')),
-)
+if django.VERSION[0] < 2:
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    urlpatterns = i18n_patterns(
+        url(r'^repanier/', include('repanier.urls')),
+        url(r'^coordi/', include(admin.site.urls)),
+        url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}},
+            name='django.contrib.sitemaps.views.sitemap'),
+        url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+        url(r'^', include('cms.urls')),
+    )
 
-urlpatterns += staticfiles_urlpatterns()
+    if settings.DEBUG:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns()
+
+else:
+
+    from django.urls import path
+
+    urlpatterns = i18n_patterns(
+        path('repanier/', include('repanier.urls')),
+        path('coordi/', admin.site.urls),
+        path('sitemap\.xml', sitemap, {'sitemaps': {'cmspages': CMSSitemap}},
+            name='django.contrib.sitemaps.views.sitemap'),
+        path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+        path('', include('cms.urls')),
+    )
+
+    if settings.DEBUG:
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+
+    urlpatterns += staticfiles_urlpatterns()
