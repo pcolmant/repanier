d21c4a19ce641c826effb88cade9653c14926229
pcolmant <pcolmant@gmail.com>
2019-06-15 20:25:42 +0200

    /repanier/rest/producers/ set safe to False on JsonResponse

--- repanier/rest/producer.py
+++ repanier/rest/producer.py
@@ -2,26 +2,31 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.fields import DecimalField
+from rest_framework.renderers import JSONRenderer
 
 from repanier.models.producer import Producer
 
 
 class ProducerSerializer(serializers.ModelSerializer):
-    minimum_order_value_amount = DecimalField(max_digits=8, decimal_places=2, source='minimum_order_value.amount',
-                                              read_only=True)
+    id = serializers.SerializerMethodField('get_short_profile_name')
+    name = serializers.SerializerMethodField('get_long_profile_name')
+    # minimum_order_value_amount = DecimalField(max_digits=8, decimal_places=2, source='minimum_order_value.amount',
+    #                                           read_only=True)
+
+    @staticmethod
+    def get_short_profile_name(obj):
+        return obj.short_profile_name
+
+    @staticmethod
+    def get_long_profile_name(obj):
+        return obj.long_profile_name
 
     class Meta:
         model = Producer
         fields = (
-            'short_profile_name',
-            'long_profile_name',
-            'email',
-            'language',
-            'phone1',
-            'vat_id',
-            'address',
-            'invoice_by_basket',
-            'minimum_order_value_amount'
+            'id',
+            'name',
+            # 'minimum_order_value_amount'
         )
 
 
@@ -33,7 +38,7 @@ def producers_list(request):
     if request.method == 'GET':
         producers = Producer.objects.filter(is_active=True)
         serializer = ProducerSerializer(producers, many=True)
-        return JsonResponse(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
     return HttpResponse(status=400)
 
 
