49cd6c9946c9763086c63781920f6b8a2a407900
Atala <alois.guillope@gmail.com>
2019-07-23 11:26:26 +0200

    refactor: move header's profile dropdown to a template

--- repanier/views/customer_invoice_class.py
+++ repanier/views/customer_invoice_class.py
@@ -25,104 +25,138 @@ class CustomerInvoiceView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(CustomerInvoiceView, self).get_context_data(**kwargs)
-        if context['object'] is None:
+        if context["object"] is None:
             # This customer has never been invoiced
-            context['bank_account_set'] = BankAccount.objects.none()
+            context["bank_account_set"] = BankAccount.objects.none()
             if settings.REPANIER_SETTINGS_SHOW_PRODUCER_ON_ORDER_FORM:
-                context['DISPLAY_PRODUCERS_ON_ORDER_FORM'] = True
+                context["DISPLAY_PRODUCERS_ON_ORDER_FORM"] = True
             else:
-                context['DISPLAY_PRODUCERS_ON_ORDER_FORM'] = False
+                context["DISPLAY_PRODUCERS_ON_ORDER_FORM"] = False
             purchase_set = Purchase.objects.none()
-            context['purchase_set'] = purchase_set
+            context["purchase_set"] = purchase_set
             purchase_by_other_set = Purchase.objects.none()
-            context['purchase_by_other_set'] = purchase_by_other_set
-            if self.request.user.is_staff:
-                raise Http404
-            else:
-                customer = self.request.user.customer
-            context['customer'] = customer
-            context['download_invoice'] = False
+            context["purchase_by_other_set"] = purchase_by_other_set
+            # if self.request.user.is_staff:
+            #     raise Http404
+            # else:
+            customer = self.request.user.customer
+            context["customer"] = customer
+            context["download_invoice"] = False
         else:
             customer_invoice = self.get_object()
-            bank_account_set = BankAccount.objects.filter(customer_invoice=customer_invoice).order_by("operation_date")
-            context['bank_account_set'] = bank_account_set
+            bank_account_set = BankAccount.objects.filter(
+                customer_invoice=customer_invoice
+            ).order_by("operation_date")
+            context["bank_account_set"] = bank_account_set
             if settings.REPANIER_SETTINGS_SHOW_PRODUCER_ON_ORDER_FORM:
-                context['DISPLAY_PRODUCERS_ON_ORDER_FORM'] = True
+                context["DISPLAY_PRODUCERS_ON_ORDER_FORM"] = True
                 purchase_set = Purchase.objects.filter(
                     customer_invoice=customer_invoice,
                     offer_item__translations__language_code=translation.get_language(),
-                    is_box_content=False
+                    is_box_content=False,
                 ).order_by("producer", "offer_item__translations__order_sort_order")
             else:
-                context['DISPLAY_PRODUCERS_ON_ORDER_FORM'] = False
+                context["DISPLAY_PRODUCERS_ON_ORDER_FORM"] = False
                 purchase_set = Purchase.objects.filter(
                     customer_invoice=customer_invoice,
-                    offer_item__translations__language_code=translation.get_language()
+                    offer_item__translations__language_code=translation.get_language(),
                 ).order_by("offer_item__translations__order_sort_order")
-            context['purchase_set'] = purchase_set
-            purchase_by_other_set = Purchase.objects.filter(
-                customer_invoice__customer_charged_id=customer_invoice.customer_id,
-                # customer_charged_id=customer_invoice.customer_id,
-                permanence_id=customer_invoice.permanence_id,
-                offer_item__translations__language_code=translation.get_language()
-            ).exclude(
-                customer_id=customer_invoice.customer_id
-            ).order_by("customer", "producer", "offer_item__translations__order_sort_order")
-            context['purchase_by_other_set'] = purchase_by_other_set
+            context["purchase_set"] = purchase_set
+            purchase_by_other_set = (
+                Purchase.objects.filter(
+                    customer_invoice__customer_charged_id=customer_invoice.customer_id,
+                    # customer_charged_id=customer_invoice.customer_id,
+                    permanence_id=customer_invoice.permanence_id,
+                    offer_item__translations__language_code=translation.get_language(),
+                )
+                .exclude(customer_id=customer_invoice.customer_id)
+                .order_by(
+                    "customer", "producer", "offer_item__translations__order_sort_order"
+                )
+            )
+            context["purchase_by_other_set"] = purchase_by_other_set
             if customer_invoice.invoice_sort_order is not None:
-                previous_customer_invoice = CustomerInvoice.objects.filter(
-                    customer_id=customer_invoice.customer_id,
-                    invoice_sort_order__isnull=False,
-                    invoice_sort_order__lt=customer_invoice.invoice_sort_order
-                ).order_by('-invoice_sort_order').only("id").first()
-                next_customer_invoice = CustomerInvoice.objects.filter(
-                    customer_id=customer_invoice.customer_id,
-                    invoice_sort_order__isnull=False,
-                    invoice_sort_order__gt=customer_invoice.invoice_sort_order
-                ).order_by('invoice_sort_order').only("id").first()
+                previous_customer_invoice = (
+                    CustomerInvoice.objects.filter(
+                        customer_id=customer_invoice.customer_id,
+                        invoice_sort_order__isnull=False,
+                        invoice_sort_order__lt=customer_invoice.invoice_sort_order,
+                    )
+                    .order_by("-invoice_sort_order")
+                    .only("id")
+                    .first()
+                )
+                next_customer_invoice = (
+                    CustomerInvoice.objects.filter(
+                        customer_id=customer_invoice.customer_id,
+                        invoice_sort_order__isnull=False,
+                        invoice_sort_order__gt=customer_invoice.invoice_sort_order,
+                    )
+                    .order_by("invoice_sort_order")
+                    .only("id")
+                    .first()
+                )
             else:
                 previous_customer_invoice = None
-                next_customer_invoice = CustomerInvoice.objects.filter(
-                    customer_id=customer_invoice.customer_id,
-                    invoice_sort_order__isnull=False
-                ).order_by('invoice_sort_order').only("id").first()
+                next_customer_invoice = (
+                    CustomerInvoice.objects.filter(
+                        customer_id=customer_invoice.customer_id,
+                        invoice_sort_order__isnull=False,
+                    )
+                    .order_by("invoice_sort_order")
+                    .only("id")
+                    .first()
+                )
             if previous_customer_invoice is not None:
-                context['previous_customer_invoice_id'] = previous_customer_invoice.id
+                context["previous_customer_invoice_id"] = previous_customer_invoice.id
             if next_customer_invoice is not None:
-                context['next_customer_invoice_id'] = next_customer_invoice.id
-            context['customer'] = customer_invoice.customer
-            context['download_invoice'] = Purchase.objects.filter(
-                customer_invoice__customer_charged_id=customer_invoice.customer_id,
-                # customer_charged_id=customer_invoice.customer_id,
-                permanence_id=customer_invoice.permanence_id,
-            ).order_by('?').exists()
+                context["next_customer_invoice_id"] = next_customer_invoice.id
+            context["customer"] = customer_invoice.customer
+            context["download_invoice"] = (
+                Purchase.objects.filter(
+                    customer_invoice__customer_charged_id=customer_invoice.customer_id,
+                    # customer_charged_id=customer_invoice.customer_id,
+                    permanence_id=customer_invoice.permanence_id,
+                )
+                .order_by("?")
+                .exists()
+            )
         return context
 
     def get_queryset(self):
         if django.VERSION[0] < 2:
-            pk = int(self.kwargs.get('pk', 0))
+            pk = int(self.kwargs.get("pk", 0))
         else:
-            pk = self.kwargs.get('pk', 0)
+            pk = self.kwargs.get("pk", 0)
         if self.request.user.is_staff:
             if pk == 0:
-                customer_id = self.request.GET.get('customer', None)
-                last_customer_invoice = CustomerInvoice.objects.filter(
-                    customer_id=customer_id, invoice_sort_order__isnull=False
-                ).only("id").order_by('-invoice_sort_order').first()
+                customer_id = self.request.GET.get("customer", None)
+                last_customer_invoice = (
+                    CustomerInvoice.objects.filter(
+                        customer_id=customer_id, invoice_sort_order__isnull=False
+                    )
+                    .only("id")
+                    .order_by("-invoice_sort_order")
+                    .first()
+                )
                 if last_customer_invoice is not None:
-                    self.kwargs['pk'] = last_customer_invoice.id
+                    self.kwargs["pk"] = last_customer_invoice.id
             return CustomerInvoice.objects.filter(
                 invoice_sort_order__isnull=False
-            ).order_by('-invoice_sort_order')
+            ).order_by("-invoice_sort_order")
         else:
             if pk == 0:
-                last_customer_invoice = CustomerInvoice.objects.filter(
-                    customer__user_id=self.request.user.id,
-                    invoice_sort_order__isnull=False
-                ).only("id").order_by('-invoice_sort_order').first()
+                last_customer_invoice = (
+                    CustomerInvoice.objects.filter(
+                        customer__user_id=self.request.user.id,
+                        invoice_sort_order__isnull=False,
+                    )
+                    .only("id")
+                    .order_by("-invoice_sort_order")
+                    .first()
+                )
                 if last_customer_invoice is not None:
-                    self.kwargs['pk'] = last_customer_invoice.id
+                    self.kwargs["pk"] = last_customer_invoice.id
             return CustomerInvoice.objects.filter(
-                customer__user_id=self.request.user.id,
-                invoice_sort_order__isnull=False
-            ).order_by('-invoice_sort_order')
+                customer__user_id=self.request.user.id, invoice_sort_order__isnull=False
+            ).order_by("-invoice_sort_order")
