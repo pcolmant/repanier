edd91358a6dfe29c3a19c8f79d7e3cbfa3339ccf
pcolmant <pcolmant@gmail.com>
2019-08-23 13:12:41 +0200

    Refactoring staff.get_str_member method

--- repanier/views/send_mail_to_coordinators_view.py
+++ repanier/views/send_mail_to_coordinators_view.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8
 import threading
 
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.forms import widgets, forms, fields, Select
+from django.forms import widgets, forms, fields
 from django.shortcuts import render
 from django.utils import translation
 from django.utils.html import strip_tags
@@ -17,23 +16,20 @@ from repanier.tools import get_repanier_template_name
 
 
 class CoordinatorsContactForm(forms.Form):
-    staff = fields.MultipleChoiceField(
-        label=_("Send an email to"),
-        choices=()
-    )
-    your_email = fields.EmailField(label=_('My email address'))
-    subject = fields.CharField(label=_('Subject'), max_length=100)
-    message = fields.CharField(label=_('Message'), widget=widgets.Textarea)
+    staff = fields.MultipleChoiceField(label=_("Send an email to"), choices=())
+    your_email = fields.EmailField(label=_("My email address"))
+    subject = fields.CharField(label=_("Subject"), max_length=100)
+    message = fields.CharField(label=_("Message"), widget=widgets.Textarea)
 
     def __init__(self, *args, **kwargs):
         super(CoordinatorsContactForm, self).__init__(*args, **kwargs)
         choices = []
         for staff in Staff.objects.filter(
-                is_active=True,
-                can_be_contacted=True,
-                translations__language_code=translation.get_language()
+            is_active=True,
+            can_be_contacted=True,
+            translations__language_code=translation.get_language(),
         ):
-            choices.append((staff.id, staff.get_title))
+            choices.append((staff.id, staff.get_str_member))
         self.fields["staff"].choices = choices
 
 
@@ -42,37 +38,34 @@ class CoordinatorsContactForm(forms.Form):
 @never_cache
 def send_mail_to_coordinators_view(request):
     template_name = get_repanier_template_name("send_mail_to_coordinators.html")
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CoordinatorsContactForm(request.POST)
         if form.is_valid():
             to_email = [request.user.email]
-            selected_staff_members = form.cleaned_data.get('staff')
+            selected_staff_members = form.cleaned_data.get("staff")
             for staff in Staff.objects.filter(
-                    is_active=True,
-                    can_be_contacted=True,
-                    id__in=selected_staff_members
-            ).order_by('?'):
+                is_active=True, can_be_contacted=True, id__in=selected_staff_members
+            ).order_by("?"):
                 to_email = list(set(to_email + staff.get_to_email))
 
             email = RepanierEmail(
-                strip_tags(form.cleaned_data.get('subject')),
-                html_body=strip_tags(form.cleaned_data.get('message')),
+                strip_tags(form.cleaned_data.get("subject")),
+                html_body=strip_tags(form.cleaned_data.get("message")),
                 to=to_email,
                 show_customer_may_unsubscribe=False,
-                send_even_if_unsubscribed=True
+                send_even_if_unsubscribed=True,
             )
             t = threading.Thread(target=email.send_email)
             t.start()
             email = form.fields["your_email"]
             email.initial = request.user.email
-            email.widget.attrs['readonly'] = True
-            return render(request, template_name,
-                          {'form': form, 'send': True})
+            email.widget.attrs["readonly"] = True
+            return render(request, template_name, {"form": form, "send": True})
     else:
         form = CoordinatorsContactForm()
 
         email = form.fields["your_email"]
         email.initial = request.user.email
-        email.widget.attrs['readonly'] = True
+        email.widget.attrs["readonly"] = True
 
-    return render(request, template_name, {'form': form, 'send': None})
+    return render(request, template_name, {"form": form, "send": None})
