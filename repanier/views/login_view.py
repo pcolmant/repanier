387e7c011495199475c7e68256c01289140b0a97
pcolmant <pcolmant@gmail.com>
2019-08-28 10:23:01 +0200

    Use "&" to append CMS_TOOLBAR_URL__EDIT_ON on login redirect if "?" is already present

--- repanier/views/login_view.py
+++ repanier/views/login_view.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8
 from cms.utils.conf import get_cms_setting
 from django.conf import settings
-from django.contrib.auth import (REDIRECT_FIELD_NAME, login as auth_login)
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import resolve_url
@@ -20,17 +20,18 @@ from repanier.tools import sint, get_repanier_template_name
 @sensitive_post_parameters()
 @csrf_protect
 @never_cache
-def login_view(request,
-               redirect_field_name=REDIRECT_FIELD_NAME,
-               authentication_form=AuthenticationForm,
-               extra_context=None):
+def login_view(
+    request,
+    redirect_field_name=REDIRECT_FIELD_NAME,
+    authentication_form=AuthenticationForm,
+    extra_context=None,
+):
     """
     Displays the login form and handles the login action.
     """
-    template_name = get_repanier_template_name('registration/login.html')
+    template_name = get_repanier_template_name("registration/login.html")
     redirect_to = request.POST.get(
-        redirect_field_name,
-        request.GET.get(redirect_field_name, EMPTY_STRING)
+        redirect_field_name, request.GET.get(redirect_field_name, EMPTY_STRING)
     )
     # Ensure the user-originating redirection url is safe.
     if not is_safe_url(url=redirect_to, allowed_hosts=request.get_host()):
@@ -52,13 +53,21 @@ def login_view(request,
             if user.is_authenticated:
 
                 if user.is_staff:
+                    if "/?" in redirect_to:
+                        concatenate_with = "&"
+                    else:
+                        concatenate_with = "?"
                     return HttpResponseRedirect(
-                        "{}?{}".format(redirect_to, get_cms_setting('CMS_TOOLBAR_URL__EDIT_ON')))
+                        "{}{}{}".format(
+                            redirect_to,
+                            concatenate_with,
+                            get_cms_setting("CMS_TOOLBAR_URL__EDIT_ON"),
+                        )
+                    )
 
                 staff_qs = Staff.objects.filter(
-                    customer_responsible_id=user.customer_id,
-                    is_active=True
-                ).order_by('?')
+                    customer_responsible_id=user.customer_id, is_active=True
+                ).order_by("?")
                 may_become_a_staff_user = staff_qs.exists()
 
                 if not may_become_a_staff_user:
@@ -69,21 +78,28 @@ def login_view(request,
 
     else:
         if user.is_authenticated:
-            as_staff_id = sint(request.GET.get('as_id', 0))
+            as_staff_id = sint(request.GET.get("as_id", 0))
 
             if as_staff_id == 0:
                 if user.is_superuser:
                     return HttpResponseRedirect(
-                        "{}?{}".format(redirect_to, get_cms_setting('CMS_TOOLBAR_URL__EDIT_ON')))
+                        "{}?{}".format(
+                            redirect_to, get_cms_setting("CMS_TOOLBAR_URL__EDIT_ON")
+                        )
+                    )
                 else:
                     # The user want to be logged in as a customer
                     return HttpResponseRedirect(redirect_to)
 
-            as_staff = Staff.objects.filter(
-                id=as_staff_id,
-                customer_responsible_id=user.customer_id,
-                is_active=True
-            ).order_by('?').first()
+            as_staff = (
+                Staff.objects.filter(
+                    id=as_staff_id,
+                    customer_responsible_id=user.customer_id,
+                    is_active=True,
+                )
+                .order_by("?")
+                .first()
+            )
 
             if as_staff is None:
                 # This should not occurs
@@ -91,11 +107,11 @@ def login_view(request,
                 return HttpResponseRedirect(redirect_to)
 
             RepanierAuthBackend.set_staff_right(
-                request=request,
-                user=user,
-                as_staff=as_staff
+                request=request, user=user, as_staff=as_staff
             )
-            return HttpResponseRedirect("{}?{}".format(redirect_to, get_cms_setting('CMS_TOOLBAR_URL__EDIT_ON')))
+            return HttpResponseRedirect(
+                "{}?{}".format(redirect_to, get_cms_setting("CMS_TOOLBAR_URL__EDIT_ON"))
+            )
 
     form = authentication_form(request)
 
@@ -103,15 +119,16 @@ def login_view(request,
         from repanier.apps import REPANIER_SETTINGS_CONFIG
 
         how_to_register = REPANIER_SETTINGS_CONFIG.safe_translation_getter(
-            'how_to_register', any_language=True, default=EMPTY_STRING)
+            "how_to_register", any_language=True, default=EMPTY_STRING
+        )
     else:
         how_to_register = EMPTY_STRING
 
     context = {
-        'form': form,
+        "form": form,
         redirect_field_name: redirect_to,
-        'how_to_register': how_to_register,
-        'staff_responsibilities': staff_responsibilities,
+        "how_to_register": how_to_register,
+        "staff_responsibilities": staff_responsibilities,
     }
     if extra_context is not None:
         context.update(extra_context)
