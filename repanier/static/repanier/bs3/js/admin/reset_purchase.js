82775dcac96576256e7a63ffa24dd7de292a6ae7
pcolmant <pcolmant@gmail.com>
2021-06-05 11:53:47 +0200

    Update admin purchase script

--- repanier/static/repanier/bs3/js/admin/reset_purchase.js
+++ repanier/static/repanier/bs3/js/admin/reset_purchase.js
@@ -1,30 +1,24 @@
 (function ($) {
     $(document).ready(function ($) {
-        if (location.pathname.indexOf('change') > -1) {
-            // Change form
-            // See : https://django-autocomplete-light.readthedocs.io/en/master/tutorial.html#clearing-autocomplete-on-forward-field-change
-            $(document).ready(function () {
-                // Bind on continent field change
-                $(':input[name$=customer]').on('change', function () {
-                    // Get the field prefix, ie. if this comes from a formset form
-                    var prefix = $(this).getFormPrefix();
+        // Change form
+        // See : https://django-autocomplete-light.readthedocs.io/en/master/tutorial.html#clearing-autocomplete-on-forward-field-change
+        // Bind on continent field change
+        $(':input[name$=customer]').on('change', function () {
+            // Get the field prefix, ie. if this comes from a formset form
+            var prefix = $(this).getFormPrefix();
 
-                    // Clear the autocomplete with the same prefix
-                    $(':input[name=' + prefix + 'delivery]').val(null).trigger('change');
-                    $(':input[name=' + prefix + 'offer_item]').val(null).trigger('change');
-                });
-            });
-            $(document).ready(function () {
-                // Bind on continent field change
-                $(':input[name$=offer_item]').on('change', function () {
-                    // Get the field prefix, ie. if this comes from a formset form
-                    var prefix = $(this).getFormPrefix();
+            // Clear the autocomplete with the same prefix
+            $(':input[name=' + prefix + 'delivery]').val(null).trigger('change');
+            $(':input[name=' + prefix + 'offer_item]').val(null).trigger('change');
+        });
+        // Bind on continent field change
+        $(':input[name$=offer_item]').on('change', function () {
+            // Get the field prefix, ie. if this comes from a formset form
+            var prefix = $(this).getFormPrefix();
 
-                    // Clear the autocomplete with the same prefix
-                    $(':input[name=' + prefix + 'quantity]').val(null).trigger('change');
-                    $(':input[name=' + prefix + 'comment]').val(null).trigger('change');
-                });
-            });
-        }
+            // Clear the autocomplete with the same prefix
+            $(':input[name=' + prefix + 'quantity]').val(null).trigger('change');
+            $(':input[name=' + prefix + 'comment]').val(null).trigger('change');
+        });
     });
 })(django.jQuery);
